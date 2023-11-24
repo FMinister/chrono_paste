d0be7b18c4226a45f1f7f7ed6b5dedb2ae8da8fd
FMinister <johannes.leyrer+git@googlemail.com>
2023-11-24 23:00:53 +0100

    update main

--- main.go
+++ main.go
@@ -6,15 +6,40 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from ChronoPaste!"))
+}
+
+func chronoView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func chronoCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		// w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/chrono/view", chronoView)
+	mux.HandleFunc("/chrono/create", chronoCreate)
 
 	log.Println("Starting server on :8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
+
+// 2.05-url-query-strings.html
