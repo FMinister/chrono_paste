18732adc244ccc028c1b134d4505bbd0ca7273c9
FMinister <johannes.leyrer+git@googlemail.com>
2023-11-26 13:39:17 +0100

    update to use port from env flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,9 +19,9 @@ func main() {
 	mux.HandleFunc("/chrono/view", chronoView)
 	mux.HandleFunc("/chrono/create", chronoCreate)
 
-	log.Println("Starting server on :8080")
+	log.Printf("Starting server on %s", *addr)
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
 
