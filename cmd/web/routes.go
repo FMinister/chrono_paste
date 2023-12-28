6bf3811d8202e1281d11fc617fdee98b30ed4ddc
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-28 23:52:43 +0100

    add auth middleware to create routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,13 +24,16 @@ func (app *application) routes() http.Handler {
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/chrono/view/:id", dynamic.ThenFunc(app.chronoView))
-	router.Handler(http.MethodGet, "/chrono/create", dynamic.ThenFunc(app.chronoCreate))
-	router.Handler(http.MethodPost, "/chrono/create", dynamic.ThenFunc(app.chronoCreatePost))
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	router.Handler(http.MethodGet, "/chrono/create", protected.ThenFunc(app.chronoCreate))
+	router.Handler(http.MethodPost, "/chrono/create", protected.ThenFunc(app.chronoCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
