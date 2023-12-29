281e1b3f4398efb3a157314a1d5d4adef3286c46
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-29 14:29:43 +0100

    add embed to use embedded filesystem instead of reading from disk

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/FMinister/chrono_paste/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -17,8 +18,8 @@ func (app *application) routes() http.Handler {
 		app.methodNotAllowed(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
