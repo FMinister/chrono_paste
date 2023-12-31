22306bb252e67eb098ff3df465c65c5eb306ddb7
FMinister <johannes.leyrer+git@googlemail.com>
2023-12-31 14:14:38 +0100

    outsource some testutils

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	statusCode, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, http.StatusOK, rs.StatusCode)
-
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	body = bytes.TrimSpace(body)
+	assert.Equal(t, http.StatusOK, statusCode)
 
 	assert.Equal(t, "OK", string(body))
 }
