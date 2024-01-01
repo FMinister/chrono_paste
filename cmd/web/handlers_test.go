63927838c67361b9970b446a6875fa2b18c357f2
FMinister <johannes.leyrer+git@googlemail.com>
2024-01-01 18:04:49 +0100

    add tests for chrono view

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -20,3 +20,61 @@ func TestPing(t *testing.T) {
 
 	assert.Equal(t, "OK", string(body))
 }
+
+func TestChronoView(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	tests := []struct {
+		name           string
+		urlPath        string
+		wantStatusCode int
+		wantBody       string
+	}{
+		{
+			name:           "Valid ID",
+			urlPath:        "/chrono/view/1",
+			wantStatusCode: http.StatusOK,
+			wantBody:       "Test Chrono",
+		},
+		{
+			name:           "Non-existent ID",
+			urlPath:        "/chrono/view/2",
+			wantStatusCode: http.StatusNotFound,
+		},
+		{
+			name:           "Negative ID",
+			urlPath:        "/chrono/view/-1",
+			wantStatusCode: http.StatusNotFound,
+		},
+		{
+			name:           "Decimal ID",
+			urlPath:        "/chrono/view/1.23",
+			wantStatusCode: http.StatusNotFound,
+		},
+		{
+			name:           "String ID",
+			urlPath:        "/chrono/view/foo",
+			wantStatusCode: http.StatusNotFound,
+		},
+		{
+			name:           "Empty ID",
+			urlPath:        "/chrono/view/",
+			wantStatusCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			statusCode, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, tt.wantStatusCode, statusCode)
+
+			if tt.wantBody != "" {
+				assert.Contains(t, string(body), tt.wantBody)
+			}
+		})
+	}
+}
